a8db1766e696aea721cd0fd95cc3807a15d25b9f
Nisticism <nistacular@gmail.com>
2017-07-27 22:23:50 -0700

    COMMIT MESSAGE

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,17 +1,67 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Window;
+import android.widget.RelativeLayout;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    Context context;
+    RecyclerView recyclerView;
+    RelativeLayout relativeLayout;
+    RecyclerView.Adapter recyclerViewAdapter;
+    RecyclerView.LayoutManager recylerViewLayoutManager;
+    String[] subjects =
+            {
+                    "ANDROID",
+                    "PHP",
+                    "BLOGGER",
+                    "WORDPRESS",
+                    "JOOMLA",
+                    "ASP.NET",
+                    "JAVA",
+                    "C++",
+                    "MATHS",
+                    "HINDI",
+                    "ENGLISH"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick (AdapterView < ? > parent, View v,
+            int position, long id){
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+                context = getApplicationContext();
+                relativeLayout = (RelativeLayout) findViewById(R.id.relativelayout1);
+                recyclerView = (RecyclerView) findViewById(R.id.recyclerview1);
+
+                recylerViewLayoutManager = new LinearLayoutManager(context);
+
+                recyclerView.setLayoutManager(recylerViewLayoutManager);
+
+                recyclerViewAdapter = new RecyclerViewAdapter(context, subjects);
+
+                recyclerView.setAdapter(recyclerViewAdapter);
+
     }
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
